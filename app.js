d2cde568c3bcbc89ed33efb5592a68c01c10f961
Felix A. Schultz <felix.schultz@engapho.com>
2024-04-11 10:19:27 +0200

    Add geolocation functionality to app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const iframe = document.querySelector("iframe");
+document.querySelector("#allowGeo").addEventListener("click", () => {
+    navigator.geolocation.getCurrentPosition(position => {
+        console.log("Latitude:", position.coords.latitude);
+        console.log("Longitude:", position.coords.longitude);
+        iframe.src = `https://www.openstreetmap.org/#map=18/${position.coords.latitude}/${position.coords.longitude}`;
+    });
+});$
