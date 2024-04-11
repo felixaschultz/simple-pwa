c2a94fd4a444085bde2a4291a8f190ac71f12170
Felix A. Schultz <felix.schultz@engapho.com>
2024-04-11 10:34:52 +0200

    Update geolocation functionality in app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ document.querySelector("#allowGeo").addEventListener("click", () => {
     navigator.geolocation.getCurrentPosition(position => {
         console.log("Latitude:", position.coords.latitude);
         console.log("Longitude:", position.coords.longitude);
-        iframe.src = `https://www.openstreetmap.org/#map=18/${position.coords.latitude}/${position.coords.longitude}`;
+        const { latitude, longitude } = position.coords;
+        iframe.src = `https://www.openstreetmap.org/export/embed.html?bbox=${longitude},${latitude}&marker=${latitude},${longitude}`;
     });
 });
